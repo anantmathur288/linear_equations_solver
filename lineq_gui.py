48ffc1ff28199ef8a550925fb17bb6f64a0c84be
Anant Mathur <85984175+anantmathur288@users.noreply.github.com>
2026-01-23 00:38:53 +0530

    Removed unnecessary dependencies

--- lineq_gui.py
+++ lineq_gui.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFrame, QLabel, QVBoxLayout, QHBoxLayout, QGridLayout, QLineEdit, QWidget, QPushButton
 from PyQt5.QtGui import QFont
-from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtCore import Qt
 from linear_equations_solver import LinearEquationSolver as LES
 from linear_equations_solver import ColumnSpaceError
 
